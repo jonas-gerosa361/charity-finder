d52c2eec72d15fe8f8a45d95c48ec0b87493d1f3
jonas-gerosa361 <ti.jonas361@gmail.com>
2021-03-25 21:59:05 -0300

    Added bootstrap and jquery for the project, next step create the frontend of the app

---
+++ resources/views/templates/master.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+        @stack('css')
+        <title>Ajuda Social</title>
+    </head>
+    <body>
+        <div>
+            @yield('content')
+        </div>
+        <script src="/assets/js/jquery.slim.min.js"></script>
+        <script src="/assets/js/popper.min.js"></script>
+        <script src="/assets/js/bootstrap.min.js"></script>
+    </body>
+</html>
